e006aef4a4141459182a8f93a17995cce76950eb
manleydj <manleydj@dukes.jmu.edu>
2013-04-30 13:09:46 -0300

    Create 252exam.js

---
+++ 252exam.js
@@ -0,0 +1,40 @@
+function validation ()
+{
+    var street = document.hello.sa.value;
+    var street2 = document.hello.sa2.value;
+    var city = document.hello.cty.value;
+    var state = document.hello.ste.value;
+    var zip = document.hello.zip.value;
+    var phonenumber = document.hello.pn.value;
+    var email = document.hello.email.value;
+    if (street == "")
+    {
+        alert ("Come on yooooo, you got to put and address here");
+    }
+    if (street2 =="")
+    {
+        alert ("Yeah dude, you need to input an address to get the ball rolling");
+    }
+    if (city =="")
+    {
+        alert ("Enter yoo Damn City Right Hurr");
+    }
+    if (state =="")
+    {
+        alert ("So what state are ya from???"); 
+    }
+    if (zip =="")
+    {
+        alert ("Forgot to enter your zip code FOOOOL.");
+    }
+    if (phonenumber =="")
+    {
+        alert ("Can I get your NUMberrr##");  
+    }
+    if(email =="")
+    {
+        alert ("enter an email address goon");
+    }
+    
+    
+}
